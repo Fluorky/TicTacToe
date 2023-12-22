d49eab934585d9a1ab55bfa4e986d2f5fb19efb9
Maciej B <mrdyrt.inf@gmail.com>
2023-12-22 12:30:34 +0100

    Making method playGame in Game class. Game is functional

--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,37 @@ public class Game {
 
     public void playGame() {
         Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Current board state:");
+            printBoard(engine.getBoardState());
 
+            Player currentPlayer = engine.getCurrentPlayer();
+            System.out.println("Player " + currentPlayer.getSymbol() + " enter your move (row and column):");
+
+            int [] move = currentPlayer.makeMove();
+            int row = move[0];
+            int col = move[1];
+
+            if(engine.makeMove(row,col,currentPlayer)){
+                if(engine.checkWin(currentPlayer)){
+                    printBoard(engine.getBoardState());
+                    System.out.println("Player "+ currentPlayer.getSymbol() + " wins!");
+                    break;
+                }
+                else if(engine.isBoardFull())
+                {
+                    System.out.println("Nobody wins!. It's a draw!");
+                    break;
+                }
+                else{
+                    engine.switchPlayer();
+                }
+            }
+            else{
+                System.out.println("Invalid move. Try again!");
+            }
+
+        }
         scanner.close();
     }
 
